c1aef7e0fe2b8029010522514cff8ccc603863f8
misterbooo <278166530@qq.com>
2019-05-02 08:34:39 +0800

    Remove Code

--- 0020-Valid-Parentheses/java-0020/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- 0094-Binary-Tree-Inorder-Traversal/java-0094/src/TreeNode.java
+++
@@ -1,7 +0,0 @@
-// Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
