d6c6cc7027587f7ba4c85179c6212bf7576ae281
Chilim <oncwnuG-QhxLc_BVJt54i2Qp4MdA@git.weixin.qq.com>
2020-04-21 20:09:10 +0800

    0771 Solved

---
+++ 0771-Jewels-Stones/Code/1.java
@@ -0,0 +1,14 @@
+
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> Jset = new HashSet();
+        for (char j: J.toCharArray())
+            Jset.add(j);
+
+        int ans = 0;
+        for (char s: S.toCharArray())
+            if (Jset.contains(s))
+                ans++;
+        return ans;
+    }
+}
